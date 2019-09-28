3451f486985deffda59db5d9f1e4d31938c877eb
dhslrl321 <dhslrl321@gmail.com>
2019-09-28 21:33:16 +0900

    dfs

--- src/stage_01_IO/Prob01_Cat.java
+++ src/stage_01_IO/Prob01_Cat.java
@@ -1,7 +1,11 @@
 package stage_01_IO;
 
+/*아래 예제와 같이 고양이를 출력하시오.*/
 public class Prob01_Cat {
     public static void main(String[] args) {
-
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
     }
 }
