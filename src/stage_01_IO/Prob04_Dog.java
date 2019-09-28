1101f3ecf34503f3a22f2d4a6f3614d9fbdc1c25
dhslrl321 <dhslrl321@gmail.com>
2019-09-28 23:51:27 +0900

    dfs

---
+++ src/stage_01_IO/Prob04_Dog.java
@@ -0,0 +1,11 @@
+package stage_01_IO;
+
+public class Prob04_Dog {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
