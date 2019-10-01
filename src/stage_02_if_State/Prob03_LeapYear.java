11573c367bbf64ef0aa0bc43fbccd556c656bd59
dhslrl321 <dhslrl321@gmail.com>
2019-10-01 19:20:38 +0900

    LeapYear

--- src/stage_02_if_State/Prob03_LeapYear.java
+++ src/stage_02_if_State/Prob03_LeapYear.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Prob03_LeapYear {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int year = input.nextInt();
 
-        if(year % 4 == 0 && year % 100 ==0){
-            System.out.println("1");
-        }else {
-            System.out.println("0");
+
+        while(true){
+            int year = input.nextInt();
+            if(year % 4 == 0 && year % 100 !=0){
+                System.out.println("1");
+            }else if(year % 400 == 0){
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
         }
+
     }
 }
