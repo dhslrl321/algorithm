6a42bd475f040ad4c7318f246a74a0118f65007b
dhslrl321 <dhslrl321@gmail.com>
2019-10-06 16:51:11 +0900

    ReversePrintStar ready

--- src/stage03_for_State/Prob10_ReversePrintStart.java
+++ src/stage03_for_State/Prob10_ReversePrintStart.java
@@ -1,4 +1,18 @@
 package stage03_for_State;
 
+import java.util.Scanner;
+
 public class Prob10_ReversePrintStart {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int count = input.nextInt();
+
+        for(int i=0; i<count; i++){
+            for(int j=0; j<count; j++){
+
+            }
+            for(int k=0; k<)
+        }
+    }
 }
