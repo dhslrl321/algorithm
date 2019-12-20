0342cd51989f84acab3cb52a2b5c86c8625850b2
dhslrl321 <dhslrl321@gmail.com>
2019-12-21 03:12:12 +0900

    For test:

--- src/stage03_for_State/Prob10_ReversePrintStart.java
+++ src/stage03_for_State/Prob10_ReversePrintStart.java
@@ -6,13 +6,8 @@ public class Prob10_ReversePrintStart {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int count = input.nextInt();
+        int a = input.nextInt();
+        System.out.println(a);
 
-        for(int i=0; i<count; i++){
-            for(int j=0; j<count; j++){
-
-            }
-            for(int k=0; k<)
-        }
     }
 }
