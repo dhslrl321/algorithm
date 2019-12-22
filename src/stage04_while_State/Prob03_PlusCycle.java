9227881163bb10461df3ce6330031ff47d84be12
dhslrl321 <dhslrl321@gmail.com>
2019-12-22 22:29:01 +0900

    일시중지

--- src/stage04_while_State/Prob03_PlusCycle.java
+++ src/stage04_while_State/Prob03_PlusCycle.java
@@ -8,15 +8,22 @@ public class Prob03_PlusCycle {
 
         int inputNum = input.nextInt();
         int left, right;
-        int temp;
-
-        if(inputNum < 10){
-            left = 0;
-            right = inputNum;
-            temp = left + right;
-        }else{
-            left = inputNum / 10;
-            right = inputNum % 10;
+        int tempNum = 0;
+        int count = 0;
+        while(tempNum != inputNum || count == 10){
+            if(inputNum < 10){
+                left = 0;
+                right = inputNum;
+                tempNum = left + right;
+                count++;
+            }else{
+                left = (tempNum / 10) * 10;
+                right = tempNum % 10;
+                
+                tempNum = left + right;
+                count++;
+            }
         }
+        System.out.println(count);
     }
 }
