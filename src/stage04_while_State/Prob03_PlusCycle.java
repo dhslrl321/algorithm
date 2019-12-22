bf6e002efbb0700c3a2c146b81257cc6d1dfe66d
dhslrl321 <dhslrl321@gmail.com>
2019-12-22 19:09:21 +0900

    3번 중단

--- src/stage04_while_State/Prob03_PlusCycle.java
+++ src/stage04_while_State/Prob03_PlusCycle.java
@@ -1,4 +1,22 @@
 package stage04_while_State;
 
+import java.util.Scanner;
+
 public class Prob03_PlusCycle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int inputNum = input.nextInt();
+        int left, right;
+        int temp;
+
+        if(inputNum < 10){
+            left = 0;
+            right = inputNum;
+            temp = left + right;
+        }else{
+            left = inputNum / 10;
+            right = inputNum % 10;
+        }
+    }
 }
