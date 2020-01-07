8d5fcdd42e4c4f25cec6b2dbe18f22cab372f0dc
dhslrl321 <dhslrl321@gmail.com>
2020-01-07 23:35:00 +0900

    add'

--- src/stage05_oneDimensionArray/Prob03_MusicalScales.java
+++ src/stage05_oneDimensionArray/Prob03_MusicalScales.java
@@ -22,7 +22,6 @@ public class Prob03_MusicalScales {
             System.out.println("descending");
         }else {
             System.out.println("mixed");
-            Arrays.toString(arr).equals(Arrays.toString(descendArr));
         }
         
     }
