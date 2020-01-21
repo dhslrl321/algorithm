206eae5936a856430b94d90bc3c88b618c8ef08e
dhslrl321 <dhslrl321@gmail.com>
2020-01-21 22:14:50 +0900

    Prob 04 first submit

--- src/stage05_oneDimensionArray/Prob04_Remainder.java
+++ src/stage05_oneDimensionArray/Prob04_Remainder.java
@@ -1,5 +1,6 @@
 package stage05_oneDimensionArray;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Prob04_Remainder {
@@ -7,8 +8,26 @@ public class Prob04_Remainder {
         /*
         *  right = 10 number get (Should be received in an array)
         *  left = 42 Fixed Number
-        *
+        *  remainder[] = 42 remainder arr
+        *  count = answer count(Dif values)
         * */
+        Scanner input = new Scanner(System.in);
+        int[] right = new int[10];
+        int[] remainder = new int[42];
+        int left = 42;
+        int countFlag = 0;
+        for(int i=0; i<right.length; i++){
+            right[i] = input.nextInt();
+            remainder[right[i] % left] = 1;
+        }
+
+        for(int i=0; i<remainder.length; i++){
+            if(remainder[i] == 1){
+                countFlag++;
+            }
+        }
+
+        System.out.println(countFlag);
 
     }
 }
