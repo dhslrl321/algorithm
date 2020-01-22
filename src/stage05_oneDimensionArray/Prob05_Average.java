9c11d29d2f80f49b2297930b2ae8af798a08f9d4
dhslrl321 <dhslrl321@gmail.com>
2020-01-22 21:33:55 +0900

    123

--- src/stage05_oneDimensionArray/Prob05_Average.java
+++ src/stage05_oneDimensionArray/Prob05_Average.java
@@ -1,5 +1,18 @@
 package stage05_oneDimensionArray;
 
 public class Prob05_Average {
-    
+    public static void main(String[] args) {
+        /*
+        *  M = maximum number he scored
+        *  Others = M * 100;
+        *
+        *  original avg = scoreSum / subject
+        *
+        *  this avg = scoreSum / subject
+        *
+        *  this score = score / max * 100
+        *
+        *  1. find maximum number
+        * */
+    }
 }
