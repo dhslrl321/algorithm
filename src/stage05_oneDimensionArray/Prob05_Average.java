50c5821fa04cd504b35e5af4136e280b4c8a13d7
dhslrl321 <dhslrl321@gmail.com>
2020-01-23 21:54:49 +0900

    Prob 05 restart

--- src/stage05_oneDimensionArray/Prob05_Average.java
+++ src/stage05_oneDimensionArray/Prob05_Average.java
@@ -8,47 +8,7 @@ import java.util.Scanner;
 
 public class Prob05_Average {
     public static void main(String[] args) {
-        /*
-        *  M = maximum number he scored
-        *  Others = M * 100;
-        *
-        *  original avg = scoreSum / subject
-        *
-        *  this avg = scoreSum / subject
-        *
-        *  this score = score / max * 100
-        *
-        *  1. find maximum number
-        *
-        *  2. 과목의 갯수 N
-        * */
-        int subjectNum = 0;
+        
 
-        Scanner input =new Scanner(System.in);
-
-        subjectNum = input.nextInt();
-
-        int[] subject = new int[subjectNum];
-
-        for(int i=0; i<subject.length; i++){
-            subject[i] = input.nextInt();
-        }
-        System.out.println("Unaligned subject[] : " + Arrays.toString(subject));
-        Arrays.sort(subject);
-        System.out.println("aligned subject[] : " + Arrays.toString(subject));
-
-        int max = subject[subject.length-1];
-        System.out.println("max : " + max);
-        int[] score = new int[subjectNum];
-
-        // issue
-        for(int i=0; i<subject.length; i++){
-            score[i] = subject[i] / max * 100;
-        }
-
-        System.out.println("score[] : " + Arrays.toString(score));
-        int avg = Arrays.stream(score).sum() / subjectNum;
-
-        System.out.println(avg);
     }
 }
