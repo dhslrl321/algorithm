9a61a4d158830432609f0550a5f61506432253b9
dhslrl321 <dhslrl321@gmail.com>
2020-01-23 22:05:53 +0900

    prob05 first submit

--- src/stage05_oneDimensionArray/Prob05_Average.java
+++ src/stage05_oneDimensionArray/Prob05_Average.java
@@ -6,9 +6,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static java.util.Arrays.stream;
+
 public class Prob05_Average {
     public static void main(String[] args) {
-        
 
+        Scanner input = new Scanner(System.in);
+
+        int subjectNum = input.nextInt();
+
+        int[] subject = new int[subjectNum];
+
+        for(int i=0; i<subject.length; i++){
+            subject[i] = input.nextInt();
+        }
+
+        Arrays.sort(subject);
+
+        int maxScore = subject[subject.length-1];
+
+        System.out.println(maxScore);
+
+        double[] score = new double[subjectNum];
+
+        for(int i=0; i<subject.length; i++){
+            score[i] = (double)subject[i] / (double)maxScore * 100;
+        }
+        System.out.println(Arrays.toString(score));
+        double avg = (Arrays.stream(score).sum() / subjectNum);
+        System.out.println(avg);
     }
 }
