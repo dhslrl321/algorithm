f6752b5841a6a23f41d5d09ea444a13c919ab893
dhslrl321 <dhslrl321@gmail.com>
2020-01-26 01:02:25 +0900

    extra - 11720 unsolved

--- src/extraStagoe/SumOfNumber_11720.java
+++ src/extraStagoe/SumOfNumber_11720.java
@@ -1,4 +1,17 @@
 package extraStagoe;
 
+import java.util.Scanner;
+
 public class SumOfNumber_11720 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int limit = input.nextInt();
+
+        int num = input.nextInt();
+        int sum = 0;
+        for(int i=0; i<limit; i++){
+            sum += String.valueOf(num).charAt(i);
+        }
+        System.out.println(sum);
+    }
 }
