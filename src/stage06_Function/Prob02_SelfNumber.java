a60c905437af3f3341b145d3276a3e1c3f37f5c1
dhslrl321 <dhslrl321@gmail.com>
2020-02-02 16:25:21 +0900

    add

--- src/stage06_Function/Prob02_SelfNumber.java
+++ src/stage06_Function/Prob02_SelfNumber.java
@@ -7,7 +7,9 @@ public class Prob02_SelfNumber {
 class SelfNumber {
     public SelfNumber() {
         for(int i=0; i<10000; i++){
-            if(i == (i + i/10 + i%10))
+            if(i + i/10 + i%10){
+
+            }
         }
     }
 }
