1d6861d9a8360220bac2247a6bb94fc59215bb9b
dhslrl321 <dhslrl321@gmail.com>
2020-02-04 14:10:21 +0900

    Prob03-String solved

--- src/stage07_String/Prob03_FindAlphabet.java
+++ src/stage07_String/Prob03_FindAlphabet.java
@@ -1,7 +1,19 @@
 package stage07_String;
 
+import java.text.Format;
+import java.util.Scanner;
+
 public class Prob03_FindAlphabet {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String s = input.next();
+
+        System.out.println(s);
+
+        for(int i=97; i<123; i++){
+            System.out.print(s.indexOf(i)+" ");
+        }
 
     }
 }
