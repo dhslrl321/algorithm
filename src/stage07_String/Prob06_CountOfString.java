470482d7f24118f3e66dd0f2d1e7e615a3f2ffc5
dhslrl321 <dhslrl321@gmail.com>
2020-02-06 01:27:56 +0900

    count of string solved + 도움 받음

--- src/stage07_String/Prob06_CountOfString.java
+++ src/stage07_String/Prob06_CountOfString.java
@@ -1,7 +1,14 @@
 package stage07_String;
 
+        import java.util.Scanner;
+        import java.util.Arrays;
+
 public class Prob06_CountOfString {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        String str = input.nextLine().trim();
+        String[] ans = str.split("\\s");
+        System.out.println(ans.length);
     }
 }
