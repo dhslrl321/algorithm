6ba1f44f667ba6128ff86af62a3d015e70b9ca49
dhslrl321 <dhslrl321@gmail.com>
2020-02-07 00:35:15 +0900

    Dial correct

--- src/stage07_String/Prob08_Dial.java
+++ src/stage07_String/Prob08_Dial.java
@@ -10,23 +10,66 @@ public class Prob08_Dial {
         int sum = 0;
         for (int i = 0; i < str.length(); i++) {
             switch(str.charAt(i)) {
-                case (65 | 66 | 67): {
+
+                case 65:
+                case 66:
+                case 67: {
                     sum += 3;
                     break;
                 }
-                case (68 | 69 | 70): {
+
+                case 68:
+                case 69:
+                case 70:{
                     sum += 4;
                     break;
                 }
-                case (71 | 72 | 73): {
+
+                case 71:
+                case 72:
+                case 73:{
                     sum += 5;
                     break;
                 }
-                case (74 | 75 | 76): {
+
+                case 74:
+                case 75:
+                case 76:{
                     sum += 6;
+                    break;
+                }
+
+                case 77:
+                case 78:
+                case 79:{
+                    sum += 7;
+                    break;
+                }
+
+                case 80:
+                case 81:
+                case 82:
+                case 83:{
+                    sum += 8;
+                    break;
+                }
+
+                case 84:
+                case 85:
+                case 86: {
+                    sum += 9;
+                    break;
+                }
+
+                case 87:
+                case 88:
+                case 89:
+                case 90: {
+                    sum += 10;
                     break;
                 }
             }
         }
+        System.out.println(sum);
     }
 }
--- src/stage07_String/Prob09_CroatiaAlphabet.java
+++ src/stage07_String/Prob09_CroatiaAlphabet.java
@@ -1,4 +1,18 @@
 package stage07_String;
 
+import java.util.Scanner;
+import java.util.Arrays;
 public class Prob09_CroatiaAlphabet {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String a = input.next();
+
+        if(a.contains("c=")){
+            System.out.println("있음");
+        }else{
+            System.out.println("no");
+        }
+
+    }
 }
