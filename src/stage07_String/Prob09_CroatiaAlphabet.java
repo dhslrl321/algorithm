f63348d73271f28222f48e4c2c5565cdb09511d1
dhslrl321 <dhslrl321@gmail.com>
2020-02-07 01:08:28 +0900

    last commit

--- src/stage07_String/Prob09_CroatiaAlphabet.java
+++ src/stage07_String/Prob09_CroatiaAlphabet.java
@@ -6,13 +6,11 @@ public class Prob09_CroatiaAlphabet {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String a = input.next();
+        String str = input.next();
+        int count = 0;
 
-        if(a.contains("c=")){
-            System.out.println("있음");
-        }else{
-            System.out.println("no");
-        }
+
+        System.out.println(count);
 
     }
 }
