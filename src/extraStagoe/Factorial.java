b8643f771d96560b121a233b327346f77d7fc9e4
dhslrl321 <dhslrl321@gmail.com>
2020-02-07 13:22:22 +0900

    Sorting aid

---
+++ src/extraStagoe/Factorial.java
@@ -0,0 +1,4 @@
+package extraStagoe;
+
+public class Factorial {
+}
---
+++ src/extraStagoe/Sorting.java
@@ -0,0 +1,25 @@
+package extraStagoe;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Sorting {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언
+
+        int arrSize = Integer.parseInt(bf.readLine());
+
+        int[] arr = new int[arrSize];
+
+        for (int i = 0; i < arrSize; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
+        }
+        Arrays.sort(arr);
+        for(int a : arr){
+            System.out.println(a);
+        }
+    }
+}
