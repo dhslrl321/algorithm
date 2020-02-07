55c813355eedc20ab1958580dbc9883070d776f1
dhslrl321 <dhslrl321@gmail.com>
2020-02-07 12:51:03 +0900

    Croatia Alphabet correct

--- src/stage07_String/Prob09_CroatiaAlphabet.java
+++ src/stage07_String/Prob09_CroatiaAlphabet.java
@@ -9,8 +9,27 @@ public class Prob09_CroatiaAlphabet {
         String str = input.next();
         int count = 0;
 
-
-        System.out.println(count);
-
+       while(true){
+            if(str.contains("c=")){
+                str = str.replace("c=", "*");
+            }else if(str.contains("c-")){
+                str = str.replace("c-", "*");
+            }else if(str.contains("dz=")){
+                str = str.replace("dz=", "*");
+            }else if(str.contains("d-")){
+                str = str.replace("d-", "*");
+            }else if(str.contains("lj")){
+                str = str.replace("lj", "*");
+            }else if(str.contains("nj")){
+                str = str.replace("nj", "*");
+            }else if(str.contains("s=")){
+                str = str.replace("s=", "*");
+            }else if(str.contains("z=")){
+                str = str.replace("z=", "*");
+            }else {
+                break;
+            }
+        }
+        System.out.println(str.length());
     }
 }
