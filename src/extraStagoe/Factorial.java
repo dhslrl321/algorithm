63c0afbaff2cb2234e80b36133954c0703a053ac
dhslrl321 <dhslrl321@gmail.com>
2020-02-07 21:54:43 +0900

    Group Word Checker correct

--- src/extraStagoe/Factorial.java
+++ src/extraStagoe/Factorial.java
@@ -2,6 +2,8 @@ package extraStagoe;
 
 public class Factorial {
     public static void main(String[] args) {
-        
+        String[] a = new String[3];
+
+        System.out.println(a.length);
     }
 }
--- src/stage07_String/Prob10_GroupWordChecker.java
+++ src/stage07_String/Prob10_GroupWordChecker.java
@@ -1,7 +1,44 @@
 package stage07_String;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Prob10_GroupWordChecker {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        Using using = new Using();
+        int strNum = input.nextInt();
+
+        int ans = 0;
+
+        for (int i = 0; i < strNum; i++) {
+            String str = input.next();
+            ans += using.checker(str);
+        }
+        System.out.println(ans);
+    }
+}
+
+class Using {
+
+    public int checker(String str){
+
+        char[] arr = str.toCharArray();
+
+        ArrayList<Character> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < str.length()-1; i++) {
+            if(arr[i] != arr[i+1]) arrayList.add(arr[i]);
+        }
+
+        arrayList.add(str.charAt(str.length()-1));
+        for (int i = 0; i < arrayList.size(); i++) {
+            for (int j = i; j < arrayList.size(); j++) {
+                if(i != j && arrayList.get(i) == arrayList.get(j)) return 0;
+            }
+        }
+
+        return 1;
     }
 }
