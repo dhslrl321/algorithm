8b898d3e501ab1e37e433ce5bb85bbb5305307db
dhslrl321 <dhslrl321@gmail.com>
2020-02-16 16:13:58 +0900

    Queue end

--- src/algorithm/class03_Queue/Prob02_Queue2.java
+++ src/algorithm/class03_Queue/Prob02_Queue2.java
@@ -1,23 +1,45 @@
 package algorithm.class03_Queue;
 
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Prob02_Queue2 {
     public static void main(String[] args) {
-        Prob02_QueueUse q = new Prob02_QueueUse();
         Scanner input = new Scanner(System.in);
 
-        System.out.println(q.queue.isEmpty());
+        int n = input.nextInt();
+
+        Prob02_QueueUse queue = new Prob02_QueueUse();
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        while(n >= 0) {
+            String stmt = input.nextLine();
+            if(stmt.length() >= 6) {
+                String[] arr = stmt.split(" ");
+                StringTokenizer st = new StringTokenizer(stmt, " ");
+                st.nextToken();
+                int num = Integer.parseInt(st.nextToken());
+                queue.push(num);
+            }
+            else if(stmt.equals("pop")) ans.add(queue.pop());
+            else if(stmt.equals("size")) ans.add(queue.size());
+            else if(stmt.equals("empty")) ans.add(queue.empty());
+            else if(stmt.equals("front")) ans.add(queue.front());
+            else if(stmt.equals("back")) ans.add(queue.back());
+            n--;
+        }
+
+        for(int res : ans) System.out.println(res);
+
     }
 }
 
 class Prob02_QueueUse {
     Queue<Integer> queue = new LinkedList<>();
 
+
     int size(){
+
         return queue.size();
     }
 
@@ -39,6 +61,12 @@ class Prob02_QueueUse {
         } catch (NoSuchElementException e) {
             return -1;
         }
+    }
+
+    int back(){
+        Object[] arr = queue.toArray();
+        if(empty() == 1) return -1;
+        else return (int) arr[arr.length-1];
     }
 
     int empty(){
