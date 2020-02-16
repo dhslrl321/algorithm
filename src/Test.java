81be1a8ccc0f31a6ed72d3fcfa3f1a22cffc6970
dhslrl321 <dhslrl321@gmail.com>
2020-02-17 02:05:02 +0900

    top fail

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,14 @@
 
-import java.util.Stack;
+import java.util.Scanner;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        Stack<String> a = new Stack<>();
-        a.push("asdf");
-        a.push("afds");
-        a.peek();
-        a.pop();
-        a.pop();
-        System.out.println(a);
-        System.out.println(a.empty());
+        int n = input.nextInt();
+
+        Stack<Integer> stack = new Stack<>();
 
     }
 }
