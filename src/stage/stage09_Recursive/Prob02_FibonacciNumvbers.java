159743569532ba8057ae5c68c175ab4d51451fac
dhslrl321 <dhslrl321@gmail.com>
2020-02-21 15:33:36 +0900

    12

---
+++ src/stage/stage09_Recursive/Prob02_FibonacciNumvbers.java
@@ -0,0 +1,18 @@
+package stage.stage09_Recursive;
+
+        import java.util.Scanner;
+
+public class Prob02_FibonacciNumvbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        System.out.println(fibonacci(num));
+    }
+
+    static int fibonacci(int num){
+        if(num >= 2) return fibonacci(num - 1) + fibonacci(num - 2);
+        else return num;
+    }
+}
+
+
