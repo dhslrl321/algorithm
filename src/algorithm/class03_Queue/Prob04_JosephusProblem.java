097663010d2487605eac42b19f8de2b4f9d9e0ea
dhslrl321 <dhslrl321@gmail.com>
2020-02-21 14:30:02 +0900

    temp stop

--- src/algorithm/class03_Queue/Prob04_JosephusProblem.java
+++ src/algorithm/class03_Queue/Prob04_JosephusProblem.java
@@ -1,4 +1,13 @@
 package algorithm.class03_Queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Prob04_JosephusProblem {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+
+    }
 }
