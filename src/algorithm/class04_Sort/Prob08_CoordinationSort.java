dd40812fb1f22a405fcda36c5dbe18b5d2f976bd
dhslrl321 <dhslrl321@gmail.com>
2020-02-24 14:10:19 +0900

    123

--- src/algorithm/class04_Sort/Prob08_CoordinationSort.java
+++ src/algorithm/class04_Sort/Prob08_CoordinationSort.java
@@ -8,13 +8,37 @@ public class Prob08_CoordinationSort {
 
         int n = input.nextInt();
 
-        int[][] coordination = new int[n][2];
+        Coodination[] coodinations = new Coodination[n];
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                coordination[i][j] = input.nextInt();
-            }
+            int x = input.nextInt();
+            int y = input.nextInt();
+            coodinations[i] = new Coodination(x, y);
         }
 
+        Arrays.sort(coodinations);
+
+
+
+    }
+}
+
+class Coodination implements Comparator<Integer>{
+    int x, y;
+
+    Coodination(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compare(Integer o1, Integer o2){
+        
+        if(o1.x > o2.x ) return -1;
+        else if(o1.x == o2.x) {
+            if(o1.y > o2.y) return -1;
+            else if(o1.y == o2.y) return 0;
+            else return 1;
+        }else return 1;
     }
 }
