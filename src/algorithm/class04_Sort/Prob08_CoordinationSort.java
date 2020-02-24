329547747891b5fe798f7604a7cf46ffce326edc
dhslrl321 <dhslrl321@gmail.com>
2020-02-24 15:17:44 +0900

    Coordination correct

--- src/algorithm/class04_Sort/Prob08_CoordinationSort.java
+++ src/algorithm/class04_Sort/Prob08_CoordinationSort.java
@@ -1,44 +1,43 @@
 package algorithm.class04_Sort;
 
+import java.nio.channels.Pipe;
 import java.util.*;
 
 public class Prob08_CoordinationSort {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         int n = input.nextInt();
 
-        Coodination[] coodinations = new Coodination[n];
-
+        Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
             int x = input.nextInt();
             int y = input.nextInt();
-            coodinations[i] = new Coodination(x, y);
+            points[i] = new Point(x, y);
         }
+        Arrays.sort(points);
 
-        Arrays.sort(coodinations);
-
-
-
+        for (int i = 0; i < n; i++) {
+            System.out.println(points[i].x + " " + points[i].y);
+        }
     }
 }
 
-class Coodination implements Comparator<Integer>{
-    int x, y;
+class Point implements Comparable<Point>{
+    Integer x, y;
 
-    Coodination(int x, int y){
+    Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
     @Override
-    public int compare(Integer o1, Integer o2){
-        
-        if(o1.x > o2.x ) return -1;
-        else if(o1.x == o2.x) {
-            if(o1.y > o2.y) return -1;
-            else if(o1.y == o2.y) return 0;
-            else return 1;
-        }else return 1;
+    public int compareTo(Point o){
+        if(this.x > o.x) return 1;
+        else if(this.x.equals(o.x)) {
+            if(this.y > o.y) return 1;
+            else if(this.y.equals(o.y)) return 0;
+            else return -1;
+        }else return -1;
     }
 }
+
+
---
+++ src/algorithm/class04_Sort/Prob09_AgeSort.java
@@ -0,0 +1,9 @@
+package algorithm.class04_Sort;
+
+public class Prob09_AgeSort {
+    public static void main(String[] args) {
+
+    }
+}
+
+
