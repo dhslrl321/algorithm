14dd3315bcf3d43e6ced1091df79eeeb892c34ba
dhslrl321 <dhslrl321@gmail.com>
2020-03-01 02:03:13 +0900

    123

---
+++ src/stage/stage09_Recursive/Prob03_Fibonacci2.java
@@ -0,0 +1,14 @@
+package stage.stage09_Recursive;
+
+import java.util.Scanner;
+
+public class Prob03_Fibonacci2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num = input.nextInt();
+
+    }
+}
+
+
