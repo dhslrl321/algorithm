b3e6f8798d14d5a98355de5fd05e2803c7cef224
dhslrl321 <dhslrl321@gmail.com>
2020-03-01 13:51:08 +0900

    s

---
+++ src/stage/stage10_Tree/Prob01_FindParent.java
@@ -0,0 +1,13 @@
+package stage.stage10_Tree;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Prob01_FindParent {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        ArrayList< ArrayList<Integer> > list = new ArrayList<ArrayList<Integer>>();
+    }
+}
