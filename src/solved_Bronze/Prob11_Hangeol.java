12a00b6c39b3544b72c733ae2ee9e4e8e929cdf6
dhslrl321 <dhslrl321@gmail.com>
2020-03-09 21:18:10 +0900

    gks

---
+++ src/solved_Bronze/Prob11_Hangeol.java
@@ -0,0 +1,8 @@
+package solved_Bronze;
+
+public class Prob11_Hangeol {
+    public static void main(String[] args) {
+        char a = '한';
+        System.out.println(Integer.parseInt(Character.toString(a)));
+    }
+}
