d708cf76c9133c6bd243bdcd43c7536d7b566f01
dhslrl321 <dhslrl321@gmail.com>
2020-03-09 18:05:50 +0900

    HugeRich

--- src/solved_Bronze/Prob02_HugeRich.java
+++ src/solved_Bronze/Prob02_HugeRich.java
@@ -14,5 +14,6 @@ public class Prob02_HugeRich {
         BigInteger mod = n.remainder(m);
         System.out.println(div);
         System.out.println(mod);
+        
     }
 }
--- src/solved_Bronze/Prob03_HexToDecimal.java
+++ src/solved_Bronze/Prob03_HexToDecimal.java
@@ -2,6 +2,6 @@ package solved_Bronze;
 
 public class Prob03_HexToDecimal {
     public static void main(String[] args) {
-        
+
     }
 }
