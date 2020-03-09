5b06768c8cbd38f0fc1ef34f6913ecf348e0a02b
dhslrl321 <dhslrl321@gmail.com>
2020-03-10 00:43:06 +0900

    123

--- src/solved_Bronze/Prob13_BigMult.java
+++ src/solved_Bronze/Prob13_BigMult.java
@@ -1,4 +1,16 @@
 package solved_Bronze;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Prob13_BigMult {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        BigInteger a = input.nextBigInteger();
+
+        BigInteger b = input.nextBigInteger();
+
+        System.out.println(a.multiply(b));
+    }
 }
