93e816e2be60f5755901bf321235ab940febbc6f
dhslrl321 <dhslrl321@gmail.com>
2020-03-09 18:52:19 +0900

    Copyright correct

---
+++ src/solved_Bronze/Prob06_copyright.java
@@ -0,0 +1,14 @@
+package solved_Bronze;
+
+import java.util.Scanner;
+
+public class Prob06_copyright {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println((a * (b-1))+1);
+    }
+}
