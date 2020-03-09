15c0edcac7797ad4d03346c08cf0451fe7779518
dhslrl321 <dhslrl321@gmail.com>
2020-03-09 19:11:27 +0900

    bignum

---
+++ src/solved_Bronze/Prob10_Join.java
@@ -0,0 +1,13 @@
+package solved_Bronze;
+
+import java.util.Scanner;
+
+public class Prob10_Join {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String n = input.next();
+
+        System.out.println(n+"??!");
+    }
+}
