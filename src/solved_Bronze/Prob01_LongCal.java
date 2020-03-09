3b5b1b685d57a1bd746d3234865760c6829181e6
dhslrl321 <dhslrl321@gmail.com>
2020-03-09 18:05:38 +0900

    LongCal

--- src/solved_Bronze/Prob01_LongCal.java
+++ src/solved_Bronze/Prob01_LongCal.java
@@ -1,4 +1,17 @@
 package solved_Bronze;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Prob01_LongCal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        BigInteger a = input.nextBigInteger();
+        BigInteger b = input.nextBigInteger();
+
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
+    }
 }
---
+++ src/solved_Bronze/Prob03_HexToDecimal.java
@@ -0,0 +1,7 @@
+package solved_Bronze;
+
+public class Prob03_HexToDecimal {
+    public static void main(String[] args) {
+        
+    }
+}
