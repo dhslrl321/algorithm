e338aa18e4d331129595a37761861c7415f623b1
dhslrl321 <dhslrl321@gmail.com>
2020-03-10 20:03:38 +0900

    AFC

---
+++ src/solved_Tier/bronze4/Prob05_AFC.java
@@ -0,0 +1,19 @@
+package solved_Tier.bronze4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Prob05_AFC {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int m = input.nextInt();
+
+        int a = (n + m) / 2;
+        int b = (n - m) / 2;
+
+        
+
+    }
+}
