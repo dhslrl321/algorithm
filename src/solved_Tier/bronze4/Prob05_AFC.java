c615ddf0336c27c6f83f75a8982924a1d8d99466
dhslrl321 <dhslrl321@gmail.com>
2020-03-10 23:51:19 +0900

    vacation

--- src/solved_Tier/bronze4/Prob05_AFC.java
+++ src/solved_Tier/bronze4/Prob05_AFC.java
@@ -1,19 +1,27 @@
 package solved_Tier.bronze4;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Prob05_AFC {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int n = input.nextInt();
-        int m = input.nextInt();
+        double n = input.nextInt();
+        double m = input.nextInt();
 
-        int a = (n + m) / 2;
-        int b = (n - m) / 2;
+        double a = (n + m) / 2;
+        double b = (n - m) / 2;
 
-        
-
+        if((n+m) % 2 != 0 || (n-m) % 2 != 0) System.out.println(-1);
+        else{
+            if(n >= 0 && n >= 0){
+                if(n >= 0 && b >= 0) {
+                    if(n >= m) System.out.println(a + " " + b);
+                    else System.out.println(a + " " + b);
+                }else System.out.println(-1);
+            }
+        }
     }
 }
