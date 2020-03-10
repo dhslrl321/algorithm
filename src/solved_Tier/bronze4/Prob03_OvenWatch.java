9ae26d88853052bbd4f9e785f75661dd4fc1ea01
dhslrl321 <dhslrl321@gmail.com>
2020-03-10 19:28:12 +0900

    OvenClock

--- src/solved_Tier/bronze4/Prob03_OvenWatch.java
+++ src/solved_Tier/bronze4/Prob03_OvenWatch.java
@@ -1,7 +1,19 @@
 package solved_Tier.bronze4;
 
+import java.util.Scanner;
+
 public class Prob03_OvenWatch {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        b += c;
+
+        a += b / 60;
+        b = (b / 60) - 
+        System.out.print(a + " " + b);
     }
 }
