cc2b0397a70ccbbb8b276f0186e090641b7581ca
dhslrl321 <dhslrl321@gmail.com>
2020-03-12 20:50:34 +0900

    asdf

--- src/Test.java
+++ src/Test.java
@@ -6,50 +6,32 @@ public class Test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        int k = input.nextInt();
-
         int[] arr = new int[n];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = input.nextInt();
         }
-        System.out.println(n / (k-1));
-       /* Arrays.sort(arr);
 
-        int min = arr[0];
-        int j;
-        *//*int check = (n - k) / (k - 1) + 1;
-        int count = 0;
-        for (int i = 0; i < check; i = i + k - 1) {
-            for (j = 0; j < k; j++) {
+        if(binarySearch(arr, 10)) System.out.println(true);
+        else System.out.println(false);
 
-                if(i == 0) arr[i] = min;
-
-                else arr[i - 1] = min;
-
-                count++;
-            }
-        }*//*
-        int check = arr.length % k;
-        int count = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if(arr.length - i == check) {
-                count++;
-                break;
-            }
-            else{
-                for (j = 0; j < k; j++) {
-
-                    arr[i] = min;
-
-                    count++;
-                }
-                i += j;
-            }
-        }*/
-
+        System.out.println("//////////////");
+        Arrays.sort(arr);
+        System.out.println(Arrays.binarySearch(arr, 10));
     }
 
+    static boolean binarySearch(int[] arr, int key){
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+
+        while(end - start >= 0){
+            mid = (start + end) / 2;
+
+            if(arr[mid] == key) return true;
+            else if(arr[mid] > key) end = mid - 1;
+            else start = mid + 1;
+        }
+        return false;
+    }
 }
---
+++ src/solved_Tier/silver5/Prob01_Brigde.java
@@ -0,0 +1,11 @@
+package solved_Tier.silver5;
+
+import java.util.Arrays;
+
+public class Prob01_Brigde {
+    public static void main(String[] args) {
+        String a = "asfd";
+
+        System.out.println(a + "asdf");
+    }
+}
