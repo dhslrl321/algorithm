99a5f6b33927fc16e86c497ac9f3424d0b4a8207
dhslrl321 <dhslrl321@gmail.com>
2020-03-13 20:16:34 +0900

    123

--- src/Test.java
+++ src/Test.java
@@ -5,34 +5,25 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int[] arr = new int[n];
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = input.nextInt();
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(queue.iterator());
+        System.out.println(list);
+        try {
+            if(queue.element() == 10) System.out.println(queue.peek());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
-
-        if(binarySearch(arr, 10)) System.out.println(true);
-        else System.out.println(false);
-
-        System.out.println("//////////////");
-        Arrays.sort(arr);
-        System.out.println(Arrays.binarySearch(arr, 10));
 
     }
 
-    static boolean binarySearch(int[] arr, int key){
-        int start = 0;
-        int end = arr.length - 1;
-        int mid;
 
-        while(end - start >= 0){
-            mid = (start + end) / 2;
-
-            if(arr[mid] == key) return true;
-            else if(arr[mid] > key) end = mid - 1;
-            else start = mid + 1;
-        }
-        return false;
-    }
 }
