171a838b4ae0e2b5a12ae466d40ea16966128d4a
dhslrl321 <dhslrl321@gmail.com>
2020-03-20 23:12:58 +0900

    123

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,22 @@
-
-import java.util.Scanner;
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Queue<Integer> queue = new LinkedList<>();
+        int n = input.nextInt();
 
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
-        queue.add(40);
+        int k = input.nextInt();
 
-        ArrayList<Integer> list = new ArrayList<>();
+        Integer[] arr = new Integer[n];
 
-        list.add(queue.iterator());
-        System.out.println(list);
-        try {
-            if(queue.element() == 10) System.out.println(queue.peek());
-        } catch (NullPointerException e) {
-            e.printStackTrace();
+        for(int i=0; i<n; i++){
+            arr[i] = input.nextInt();
         }
 
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        int cost = arr[k-1] - arr[arr.length-1];
+        System.out.println(cost);
     }
-
-
 }
