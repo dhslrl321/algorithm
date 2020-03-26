f844ed66e6213e57e7f97df51ae10a3957350c1b
dhslrl321 <dhslrl321@gmail.com>
2020-03-26 20:33:12 +0900

    123

--- src/stage/stage11_DynamicPrograming/Prob03_2nTiling.java
+++ src/stage/stage11_DynamicPrograming/Prob03_2nTiling.java
@@ -1,7 +1,31 @@
 package stage.stage11_DynamicPrograming;
 
-public class Prob03_2nTiling {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Prob03_2nTiling {
+
+    static int[] memo;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        memo = new int[n+1];
+        System.out.println(dp(n));
     }
+
+    private static int dp(int n){
+
+        if(memo[n] > 0) return memo[n];
+
+        memo[1] = 1;
+        memo[2] = 2;
+        memo[n] = memo[n-1] + memo[n-2];
+
+        return memo[n];
+    }
+
+
 }
---
+++ src/stage/stage11_DynamicPrograming/Prob03_2nTilingBTP.java
@@ -0,0 +1,7 @@
+package stage.stage11_DynamicPrograming;
+
+public class Prob03_2nTilingBTP {
+    public static void main(String[] args) {
+        
+    }
+}
