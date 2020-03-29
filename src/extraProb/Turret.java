83853830f8cebde48c01732abcdbbb45e0f7f41c
dhslrl321 <dhslrl321@gmail.com>
2020-03-30 00:52:54 +0900

    turret solved

--- src/extraProb/Turret.java
+++ src/extraProb/Turret.java
@@ -1,51 +1,37 @@
 package extraProb;
 
-import java.io.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class Turret {
-    public static void main(String[] args) throws IOException{
-        Scanner input = new Scanner(System.in);
-        int t = input.nextInt();
-        int[] x1 = new int[t];
-        int[] x2 = new int[t];
-        int[] y1 = new int[t];
-        int[] y2 = new int[t];
-        int[] r1 = new int[t];
-        int[] r2 = new int[t];
+    public static void main(String [] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int res[] = new int[T];
+        for (int i = 0 ; i < T ; i++){
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int r1 = sc.nextInt();
 
-        for (int i = 0; i < t; i++) {
-            x1[i] = input.nextInt();
-            x2[i] = input.nextInt();
-            y1[i] = input.nextInt();
-            y2[i] = input.nextInt();
-            r1[i] = input.nextInt();
-            r2[i] = input.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            int r2 = sc.nextInt();
+            res[i] = solve(x1, y1, r1, x2, y2, r2);
         }
+        for (int i = 0 ; i < T ; i++)
+            System.out.println(res[i]);
 
+        sc.close();
+    }
 
-        // x, y좌표 구하기
-        for (int i = 0; i < t; i++) {
+    public static int solve(int x1, int y1, int r1, int x2, int y2, int r2) {
+        double r = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
 
-            double x = Math.pow(x2[i], 2) - Math.pow(x1[i], 2);
-            double y = Math.pow(y2[i], 2) - Math.pow(y1[i], 2);
-
-            int xy = (int)Math.sqrt(x+y);
-            int sum = r1[i] + r2[i];
-            if(x1[i] == x2[i] && y1[i] == y2[i] && r1[i] == r2[i]) System.out.println(-1);
-            else if(sum == xy) System.out.println(1);
-            else if(sum > xy) System.out.println(2);
-            else System.out.println(0);
-
-        }
-
-        // 빗변 구하기
-
-        // r 값 비교문 만들기
-
-        // 출력
-
+        if (x1 == x2 && y1 == y2 && r1 == r2)
+            return -1;
+        else if (r > r1 + r2 || x1 == x2 && y1 == y2 && r1 != r2 ||  r < Math.abs(r1-r2))
+            return 0;
+        else if (r == r1 + r2 || Math.abs(r1-r2) == r)
+            return 1;
+        return 2;
     }
 }
