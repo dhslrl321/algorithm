f2abadaf5a99a251029dfb0741999bbbd699a159
dhslrl321 <dhslrl321@gmail.com>
2020-04-01 17:01:01 +0900

    123

--- src/stage/stage11_DynamicPrograming/Prob06_CardBuying.java
+++ src/stage/stage11_DynamicPrograming/Prob06_CardBuying.java
@@ -1,7 +1,14 @@
 package stage.stage11_DynamicPrograming;
 
+import java.io.*;
+
 public class Prob06_CardBuying {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        output.flush();
+        output.close();
 
     }
 }
