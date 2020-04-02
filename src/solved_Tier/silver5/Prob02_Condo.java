75be6e2a7e468b0e96d7824168f28cb8a6398d64
dhslrl321 <dhslrl321@gmail.com>
2020-04-02 10:02:31 +0900

    콘도 선정

---
+++ src/solved_Tier/silver5/Prob02_Condo.java
@@ -0,0 +1,16 @@
+package solved_Tier.silver5;
+
+import java.io.*;
+
+public class Prob02_Condo {
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+
+        output.write("\n");
+        output.flush();
+        output.close();
+    }
+}
