f0af0edbd17690407273ac9631acae64d73c63c8
dhslrl321 <dhslrl321@gmail.com>
2020-04-02 17:59:52 +0900

    123

--- src/stage/stage11_DynamicPrograming/Prob06_CardBuying.java
+++ src/stage/stage11_DynamicPrograming/Prob06_CardBuying.java
@@ -1,22 +1,31 @@
 package stage.stage11_DynamicPrograming;
 
 import java.io.*;
+import java.util.*;
 
 public class Prob06_CardBuying {
 
+    static int[] arr;
     static int[] memo = new int[10001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int n = Integer.parseInt(input.readLine());
+
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        arr = new int[n];
+        int i = 0;
+
+        while(st.hasMoreTokens()){
+            arr[i] = Integer.parseInt(st.nextToken());
+            i++;
+        }
+
 
         output.write("\n");
         output.flush();
         output.close();
-    }
-
-    private static void d(){
-
     }
 }
