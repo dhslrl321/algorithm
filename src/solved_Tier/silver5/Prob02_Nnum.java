57519bbcd1e4f99cb12020b2aa935e254cf40bc6
dhslrl321 <dhslrl321@gmail.com>
2020-04-02 10:45:27 +0900

    n번쨰 큰 수 완료

--- src/solved_Tier/silver5/Prob02_Nnum.java
+++ src/solved_Tier/silver5/Prob02_Nnum.java
@@ -1,6 +1,7 @@
 package solved_Tier.silver5;
 
 import java.io.*;
+import java.util.*;
 
 public class Prob02_Nnum {
     public static void main(String[] args) throws IOException{
@@ -8,8 +9,20 @@ public class Prob02_Nnum {
 
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int t = Integer.parseInt(input.readLine());
+        int[] arr = new int[10];
+        int j = 0;
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(input.readLine());
+            j = 0;
+            while(st.hasMoreTokens()){
+                arr[j] = Integer.parseInt(st.nextToken());
+                j++;
+            }
+            Arrays.sort(arr);
+            output.write(arr[7] + "\n");
+        }
 
-        output.write("\n");
         output.flush();
         output.close();
     }
