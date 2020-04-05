40f548d06da31a3a446ec440d351323db56d8df3
dhslrl321 <dhslrl321@gmail.com>
2020-04-05 16:42:13 +0900

    ss

--- src/stage/stage11_DynamicPrograming/Prob06_CardBuying.java
+++ src/stage/stage11_DynamicPrograming/Prob06_CardBuying.java
@@ -1,32 +1,31 @@
 package stage.stage11_DynamicPrograming;
 
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Prob06_CardBuying {
 
-    static int[] arr;
-    static int[] memo = new int[10001];
-
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(input.readLine());
-        StringTokenizer st = new StringTokenizer(input.readLine());
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n+1];
+        int[] d = new int[n+1];
+
         for (int i = 1; i < n+1; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] d = new int[n+1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 d[i] = Math.max(d[i], d[i-j] + arr[j]);
             }
         }
-        output.write(d[n] + "\n");
-        output.flush();
-        output.close();
+
+        bw.write(d[n] + "\n");
+        bw.flush();
+        bw.close();
     }
 }
--- src/stage/stage11_DynamicPrograming/Prob10_BuyingCard.java
+++ src/stage/stage11_DynamicPrograming/Prob10_BuyingCard.java
@@ -1,4 +1,40 @@
 package stage.stage11_DynamicPrograming;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
 public class Prob10_BuyingCard {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        int[] d = new int[n+1];
+
+        //Arrays.fill(d, 1000 * 10000);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            d[i] = 1000 * 10000;
+        }
+        //d[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                d[i] = Math.min(d[i], d[i-j] + arr[j]);
+            }
+        }
+
+        bw.write(d[n] + "\n");
+
+        bw.flush();
+        bw.close();
+    }
 }
