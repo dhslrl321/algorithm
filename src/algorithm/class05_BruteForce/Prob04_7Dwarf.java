ede60a8fd13d7b19294fb4ace411c59eb85cc780
dhslrl321 <dhslrl321@gmail.com>
2020-04-07 20:07:31 +0900

    123

--- src/algorithm/class05_BruteForce/Prob04_7Dwarf.java
+++ src/algorithm/class05_BruteForce/Prob04_7Dwarf.java
@@ -1,43 +1,27 @@
 package algorithm.class05_BruteForce;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
 
 public class Prob04_7Dwarf {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int[] dwarfs = new int[9];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int i = 0; i < 9; i++) {
-            dwarfs[i] = input.nextInt();
+        int[] arr = new int[10];
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
-        Arrays.sort(dwarfs);
-        int[] ans = new int[7];
-        for (int i = 0; i < dwarfs.length-6; i++) {
-            for (int j = 0; j < dwarfs.length-5; j++) {
-                for (int k = 0; k < dwarfs.length-4; k++) {
-                    for (int l = 0; l < dwarfs.length-3; l++) {
-                        for (int m = 0; m < dwarfs.length-2; m++) {
-                            for (int n = 0; n < dwarfs.length-1; n++) {
-                                for (int o = 0; o < dwarfs.length; o++) {
-                                    if(dwarfs[i] + dwarfs[j] + dwarfs[k] + dwarfs[l] + dwarfs[m] + dwarfs[n] + dwarfs[o] == 100){
-                                        ans[0] = dwarfs[i];
-                                        ans[1] = dwarfs[j];
-                                        ans[2] = dwarfs[k];
-                                        ans[3] = dwarfs[l];
-                                        ans[4] = dwarfs[m];
-                                        ans[5] = dwarfs[n];
-                                        ans[6] = dwarfs[o];
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length; j++) {
+                if(arr[i] - arr[j] - sum == 100) bw.write()
             }
         }
-        Arrays.sort(ans);
-        for ( int iter : ans) System.out.println(iter);
+
+
+        bw.flush();
+        bw.close();
     }
 }
