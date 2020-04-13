800838e90ce816e6563dfaae0b716a2c621327b0
dhslrl321 <dhslrl321@gmail.com>
2020-04-13 22:28:07 +0900

    123

--- src/Test.java
+++ src/Test.java
@@ -6,16 +6,29 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        long n = 12345678912L;
-        String str = Long.toString(n);
-        String[] temp = str.split("");
-        BigInteger num = BigInteger.valueOf(n);
-        int[] answer = new int[temp.length];
-        for (int i = 0; i < temp.length; i++) {
-            answer[i] = num.mod(BigInteger.valueOf(10));
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
         }
 
+        int[] ary = new int[arr.length];
+        boolean[] array = new boolean[arr.length + 1];
 
-        for(int iter : answer) System.out.print(iter+" ");
+        for (int i = 0; i < arr.length; i++) {
+            int a = arr[i];
+
+            if(!array[a]) {
+                array[a] = true;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i])
+                System.out.print(i + " ");
+        }
     }
 }
