bc3e057172c62c8543b03aa16e2ab033e7194017
dhslrl321 <dhslrl321@gmail.com>
2020-04-19 19:02:50 +0900

    su

---
+++ src/programmers/level1/Prob03_ImportantTest.java
@@ -0,0 +1,17 @@
+package programmers.level1;
+
+import java.util.*;
+
+public class Prob03_ImportantTest {
+    public static void main(String[] args) {
+        String a = "asdf";
+        char[] word = a.toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+
+
+        sb.append("b");
+
+        System.out.println(sb.toString());
+    }
+}
