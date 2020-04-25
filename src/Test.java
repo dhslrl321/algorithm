593231e1c72a50cb3119fa3918c8ba853a4f8a63
dhslrl321 <dhslrl321@gmail.com>
2020-04-26 04:14:05 +0900

    123

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,5 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
     }
 }
--- src/stage/stage12_Greedy/Prob04_Lost.java
+++ src/stage/stage12_Greedy/Prob04_Lost.java
@@ -1,13 +1,25 @@
 package stage.stage12_Greedy;
 
 import java.io.*;
+import javax.script.*;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Prob04_Lost {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        
+        String exp = br.readLine();
+        StringTokenizer st = new StringTokenizer(exp, "+-");
+        char[] arr = exp.toCharArray();
+        List<String> list = new ArrayList<>();
+
+        while(st.hasMoreTokens()){
+            list.add(st.nextToken());
+        }
+
+
 
         bw.flush();
         bw.close();
