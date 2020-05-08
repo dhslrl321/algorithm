3fc35256289972f089ee7d5c11b713eb34c57971
dhslrl321 <dhslrl321@gmail.com>
2020-05-08 21:46:05 +0900

    123

--- src/algorithm/class02_Stack/Prob07_PostMarkingFormula.java
+++ src/algorithm/class02_Stack/Prob07_PostMarkingFormula.java
@@ -1,13 +1,26 @@
 package algorithm.class02_Stack;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Prob07_PostMarkingFormula {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), "", true);
+        String expression = br.readLine();
+
+        List<Character> answer = new ArrayList<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char word = expression.charAt(i);
+            if(word >= 65 && word <= 90){
+                answer.add(word);
+            } else {
+                // Push to Stack
+            }
+        }
+        bw.write(answer.toString());
+
         bw.flush();
         bw.close();
     }
