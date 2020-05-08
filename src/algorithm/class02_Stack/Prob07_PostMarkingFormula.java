6e94418e4b9554d921b7e4786cb3f5ccb2d0998a
dhslrl321 <dhslrl321@gmail.com>
2020-05-08 20:27:31 +0900

    gold 4 postmarking formula

--- src/algorithm/class02_Stack/Prob07_PostMarkingFormula.java
+++ src/algorithm/class02_Stack/Prob07_PostMarkingFormula.java
@@ -1,4 +1,14 @@
 package algorithm.class02_Stack;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Prob07_PostMarkingFormula {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "", true);
+        bw.flush();
+        bw.close();
+    }
 }
