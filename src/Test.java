2b6dc2ca0c3e2b2a38a8dc554e7b045e2b63cc8b
dhslrl321 <dhslrl321@gmail.com>
2020-06-04 02:09:19 +0900

    quick sort refresh

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,45 @@
-import javax.print.DocFlavor;
-import java.awt.event.InputEvent;
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        int[] arr = {5, 2, 9, 1, 8 ,7, 4, 3, 6};
+        quickSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /*quick for call*/
+    private static void quickSort(int[] arr){
+        quickSort(arr, 0, arr.length-1);
+    }
+
+    /*quick sort for logic*/
+    private static void quickSort(int[] arr, int start, int end){
+        int part = partition(arr, start, end);
+        if(start < part - 1) quickSort(arr, start, part - 1);
+        if(part < end) quickSort(arr, part, end);
+    }
+
+    /*partition method*/
+    private static int partition(int[] arr, int start, int end){
+        int pivot = arr[(start + end) / 2];
+        while(start <= end){
+            while(arr[start] < pivot) start++;
+            while(arr[end] > pivot) end--;
+
+            if(start <= end){
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+    /*swap method*/
+    private static int[] swap(int[] arr, int idx1, int idx2){
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+        return arr;
     }
 }
--- src/algorithm/class04_Sort/Prob11_KthNumber.java
+++ src/algorithm/class04_Sort/Prob11_KthNumber.java
@@ -1,45 +1,45 @@
 package algorithm.class04_Sort;
 
+import java.io.*;
 import java.util.*;
 
 public class Prob11_KthNumber {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-        int n = input.nextInt();
-        int k = input.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[Integer.parseInt(st.nextToken())];
+        int k = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = input.nextInt();
+        String elements = br.readLine();
+        st = new StringTokenizer(elements);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         quickSort(arr);
-
-
-        System.out.println(arr[k-1]);
+        bw.write(String.valueOf(arr[k-1]));
+        bw.flush();
+        bw.close();
     }
 
     private static void quickSort(int[] arr){
-        quickSort(arr, 0, arr.length-1);
+        quickSort(arr, 0, arr.length - 1);
     }
-
     private static void quickSort(int[] arr, int start, int end){
         int part2 = partitioning(arr, start, end);
-        //if(part2 == k) return arr[k]
         if(start < part2 - 1) quickSort(arr, start, part2 - 1);
-        if(part2 - 1 > end) quickSort(arr, part2, end);
+        if(part2 < end) quickSort(arr, part2, end);
     }
 
-    private static int partitioning(int[] arr, int start, int end){
+    private static int partitioning(int[] arr, int start, int end) {
         int pivot = arr[(start + end) / 2];
-
         while(start <= end){
             while(arr[start] < pivot) start++;
             while(arr[end] > pivot) end--;
-            if(start<=end){
+            if(start <= end){
                 swap(arr, start, end);
                 start++;
                 end--;
@@ -53,4 +53,6 @@ public class Prob11_KthNumber {
         arr[idx1] = arr[idx2];
         arr[idx2] = temp;
     }
+
+
 }
