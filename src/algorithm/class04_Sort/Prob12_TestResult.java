dde7f552725a5e4ca9a0ad2a75d14cd95621393c
dhslrl321 <dhslrl321@gmail.com>
2020-06-09 11:38:02 +0900

    algorightm 문제 생성'

---
+++ src/algorithm/class04_Sort/Prob12_TestResult.java
@@ -0,0 +1,16 @@
+package algorithm.class04_Sort;
+
+
+import jdk.jfr.Frequency;
+
+import java.io.*;
+
+public class Prob12_TestResult {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        
+
+    }
+}
