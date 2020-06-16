1f538f3b3d9babccff09266522fb648be99a0af7
dhslrl321 <dhslrl321@gmail.com>
2020-06-16 21:53:07 +0900

    hanoi tower

--- src/algorithm/class09_Recursion/Prob01_Hanoi.java
+++ src/algorithm/class09_Recursion/Prob01_Hanoi.java
@@ -1,7 +1,16 @@
 package algorithm.class09_Recursion;
 
 public class Prob01_Hanoi {
+    static int count = 0;
     public static void main(String[] args) {
-        
+
+    }
+    static void hanoi(int n, int start, int end, int temp){
+        if(n == 1){
+            System.out.println(start + "->" + end);
+            count++;
+            return;
+        }
+
     }
 }
