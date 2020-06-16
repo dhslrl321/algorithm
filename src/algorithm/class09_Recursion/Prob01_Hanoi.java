d3753144c60899b81d18117401b1d910847f0f1a
dhslrl321 <dhslrl321@gmail.com>
2020-06-16 23:18:27 +0900

    hanoitower biginteger 해결

--- src/algorithm/class09_Recursion/Prob01_Hanoi.java
+++ src/algorithm/class09_Recursion/Prob01_Hanoi.java
@@ -1,16 +1,39 @@
 package algorithm.class09_Recursion;
 
-public class Prob01_Hanoi {
-    static int count = 0;
-    public static void main(String[] args) {
+import java.math.BigInteger;
+import java.util.Scanner;
 
-    }
-    static void hanoi(int n, int start, int end, int temp){
-        if(n == 1){
-            System.out.println(start + "->" + end);
-            count++;
-            return;
+public class Prob01_Hanoi {
+    static BigInteger count = BigInteger.ZERO;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        hanoi(n, 1, 3, 2);
+        if(n < 21){
+            System.out.println(count);
+            System.out.println(sb.toString());
+        }else {
+            System.out.println(count);
         }
 
+    }
+
+    /*20 under*/
+    static void hanoi(int n, int start, int end, int temp){
+        count = count.add(BigInteger.valueOf(1));
+        if(n == 1){
+            sb.append(start);
+            sb.append(" ");
+            sb.append(end);
+            sb.append("\n");
+            return;
+        }
+        hanoi(n-1, start, temp, end);
+        sb.append(start);
+        sb.append(" ");
+        sb.append(end);
+        sb.append("\n");
+        hanoi(n-1, temp, end, start);
     }
 }
