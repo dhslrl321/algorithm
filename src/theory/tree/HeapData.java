f607faa9fe93a0ee8d22b79ccb32f8a419745306
dhslrl321 <dhslrl321@gmail.com>
2020-06-24 14:00:33 +0900

    heap data structure

--- src/theory/tree/HeapData.java
+++ src/theory/tree/HeapData.java
@@ -1,4 +1,33 @@
 package theory.tree;
 
+import java.util.ArrayList;
+
 public class HeapData {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(10);
+        System.out.println(list.size());
+    }
+}
+
+class MinHeap {
+    private ArrayList<Integer> heap;
+
+    /*heap init*/
+    public MinHeap(){
+        heap = new ArrayList<>();
+        heap.add(0); // heap 의 인덱스는 알기 쉽게 0부터 시작한다는 특성을 따른다.
+    }
+}
+
+
+    // insertion
+    private void insert(int data) {
+        heap.add(data);
+        int position = heap.size() - 1;
+        while(position > 1 && pasdf)
+    }
+class MaxHeap {
+
 }
