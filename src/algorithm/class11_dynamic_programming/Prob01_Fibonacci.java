7a392bd353eb81b9ed188b038a8e4b604461a75d
dhslrl321 <dhslrl321@gmail.com>
2020-06-25 00:50:54 +0900

    01tailing

---
+++ src/algorithm/class11_dynamic_programming/Prob01_Fibonacci.java
@@ -0,0 +1,26 @@
+package algorithm.class11_dynamic_programming;
+
+import java.util.*;
+import java.io.*;
+
+public class Prob01_Fibonacci {
+    static long[] fibo;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        fibo = new long[n + 1];
+        fibo[0] = 0L;
+        fibo[1] = 1L;
+        fibo[2] = 1L;
+
+        for (int i = 3; i <= n; i++) {
+            fibo[i] = fibo[i-1] + fibo[i-2];
+        }
+        bw.write(String.valueOf(fibo[n]));
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/algorithm/class11_dynamic_programming/Prob02_01Tailing.java
@@ -0,0 +1,4 @@
+package algorithm.class11_dynamic_programming;
+
+public class Prob02_01Tailing {
+}
