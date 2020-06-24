0cf9ae2127e5953d874147cea96dd50ef6dfcad7
dhslrl321 <dhslrl321@gmail.com>
2020-06-24 15:25:30 +0900

    백준 최소힙 문제 생성

---
+++ src/algorithm/class10_Tree/Prob02_MinHeap.java
@@ -0,0 +1,19 @@
+package algorithm.class10_Tree;
+
+import java.io.*;
+import java.util.*;
+
+public class Prob02_MinHeap {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        fori
+
+        bw.flush();
+        bw.close();
+    }
+}
