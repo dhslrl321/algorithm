51c5da34973eb9a569df42c51f39caab4143e6f2
dhslrl321 <dhslrl321@gmail.com>
2020-06-24 23:27:21 +0900

    asdf

---
+++ src/algorithm/class10_Tree/Prob03_MaxHeap.java
@@ -0,0 +1,9 @@
+package algorithm.class10_Tree;
+
+import java.util.PriorityQueue;
+
+public class Prob03_MaxHeap {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+    }
+}
