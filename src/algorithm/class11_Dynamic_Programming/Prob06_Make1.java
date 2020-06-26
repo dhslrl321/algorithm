8b2f183b7eac453df0a0033d17ab3a56de17020c
dhslrl321 <dhslrl321@gmail.com>
2020-06-27 01:12:14 +0900

    체육복

---
+++ src/algorithm/class11_Dynamic_Programming/Prob06_Make1.java
@@ -0,0 +1,28 @@
+package algorithm.class11_Dynamic_Programming;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Prob06_Make1 {
+    static int[] d = new int[1000001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        d[1] = 0;
+        d[2] = 1;
+        d[3] = 1;
+
+        for (int i = 3; i < n+1; i++) {
+            d[i] = d[i-1] + 1;
+            if(i % 2 == 0) d[i] = Math.min(d[i/2] + 1, d[i]);
+            if(i % 3 == 0) d[i] = Math.min(d[i/3] + 1, d[i]);
+        }
+
+        bw.write(String.valueOf(d[n]));
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/algorithm/class12_Greedy/Prob03_ATM.java
@@ -0,0 +1,35 @@
+package algorithm.class12_Greedy;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Prob03_ATM {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int count = 0;
+        int acc = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                count += arr[j];
+            }
+        }
+
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+    }
+}
+
+
