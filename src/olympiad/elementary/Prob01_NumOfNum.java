8924b5a4327c10d736eb1adf44b3b39d2df7c724
dhslrl321 <dhslrl321@gmail.com>
2020-06-28 16:02:24 +0900

    num of num solved

--- src/olympiad/elementary/Prob01_NumOfNum.java
+++ src/olympiad/elementary/Prob01_NumOfNum.java
@@ -1,4 +1,26 @@
 package olympiad.elementary;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Prob01_NumOfNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        String num = String.valueOf(a * b * c);
+        String[] arr = num.split("");
+        int[] count = new int[10];
+
+        for (String s : arr) {
+            count[Integer.parseInt(s)]++;
+        }
+
+        for(int iter : count) {
+            System.out.println(iter);
+        }
+    }
 }
