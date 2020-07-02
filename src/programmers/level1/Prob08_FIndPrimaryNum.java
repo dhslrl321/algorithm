b756214400ef41e50bb42fab76fcd4a142c949f2
dhslrl321 <dhslrl321@gmail.com>
2020-07-02 13:17:03 +0900

    이상한 숫자 만들기

--- src/programmers/level1/Prob08_FIndPrimaryNum.java
+++ src/programmers/level1/Prob08_FIndPrimaryNum.java
@@ -1,4 +1,29 @@
 package programmers.level1;
 
+import java.util.Arrays;
+
 public class Prob08_FIndPrimaryNum {
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.println(solution(n));
+
+    }
+
+    private static int solution(int n){
+        boolean[] arr = new boolean[n + 1];
+        int answer = 0;
+
+        for (int i = 2; i < arr.length; i++) {
+            if(i % 2 == 0 || i % 3 == 0 || i % 5 == 0){
+                if(i == 2 || i == 3 || i ==5) arr[i] = false;
+                else {
+                    arr[i] = true;
+                }
+            }
+        }
+        for(boolean iter : arr) {
+            if(!iter) answer++;
+        }
+        return answer - 2;
+    }
 }
---
+++ src/programmers/level1/Prob09_MakeWeiredWord.java
@@ -0,0 +1,7 @@
+package programmers.level1;
+
+public class Prob09_MakeWeiredWord {
+    public static void main(String[] args) {
+
+    }
+}
