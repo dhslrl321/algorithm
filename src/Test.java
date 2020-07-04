6a27cadb88f94a4e52edb08d5a8ebce1f0089365
dhslrl321 <dhslrl321@gmail.com>
2020-07-04 13:15:41 +0900

    baekjoon

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,15 @@
 
 public class Test {
     public static void main(String[] args) {
+        String a = "a ";
+        String b = " b ";
+        String c = " c";
 
+        String abc = a+b+c;
+        System.out.println(abc);
+        String d = "asdf";
+        abc += d;
+        System.out.println(abc);
     }
 
 }
---
+++ src/baekjoon/classification/class13_Implementation/Prob03_FoodManager.java
@@ -0,0 +1,18 @@
+package baekjoon.classification.class13_Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Prob03_FoodManager {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int m = input.nextInt();
+        int k = 0;
+    }
+}
