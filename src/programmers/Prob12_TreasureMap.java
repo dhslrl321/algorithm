d71ef06d38dfda721d940059d95e6e0a3440c485
dhslrl321 <dhslrl321@gmail.com>
2020-07-06 02:33:47 +0900

    test'

--- src/programmers/Prob12_TreasureMap.java
+++ src/programmers/Prob12_TreasureMap.java
@@ -1,4 +1,11 @@
 package programmers;
 
 public class Prob12_TreasureMap {
+    public static void main(String[] args) {
+        int[] arr1 =
+    }
+
+    private static void solution(){
+
+    }
 }
