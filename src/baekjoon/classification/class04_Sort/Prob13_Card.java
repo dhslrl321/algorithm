0d1f541fae0e95fa1c9830d313a0440bbb0ee552
dhslrl321 <dhslrl321@gmail.com>
2020-07-05 13:33:22 +0900

    test

--- src/baekjoon/classification/class04_Sort/Prob13_Card.java
+++ src/baekjoon/classification/class04_Sort/Prob13_Card.java
@@ -1,7 +1,33 @@
 package baekjoon.classification.class04_Sort;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Prob13_Card {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        Integer[] intArr = new Integer[n];
+        for(int i = 0; i < n; i++){
+            intArr[i] = input.nextInt();
+        }
+
+        Arrays.sort(intArr, Collections.reverseOrder());
+
+        Long[] longArrPos = new Long[intArr[0] + 1];
+        Long[] longArrNeg = new Long[intArr[0] + 1];
+
+        for(Integer iter : intArr){
+            if(iter >= 0) longArrPos[iter]++;
+            else longArrNeg[-iter]++;
+        }
+        Arrays.sort(longArrPos, Collections.reverseOrder());
+        Arrays.sort(longArrNeg, Collections.reverseOrder());
+
+        if(longArrPos[0] > longArrNeg[0]){
+            System.out.println();
+        }
     }
 }
