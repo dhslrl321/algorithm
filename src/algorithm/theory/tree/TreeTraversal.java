0578074bbfcbb193a66bc659ceb2d6b427045446
dhslrl321 <dhslrl321@gmail.com>
2020-07-09 03:58:54 +0900

    traversal tree update

--- src/algorithm/theory/tree/TreeTraversal.java
+++ src/algorithm/theory/tree/TreeTraversal.java
@@ -1,4 +1,58 @@
 package algorithm.theory.tree;
 
 public class TreeTraversal {
+    public static void main(String[] args) {
+
+    }
+}
+
+class TraversalNode{
+    int data;
+    TraversalNode rightNode;
+    TraversalNode leftNode;
+}
+
+class TraversalTree{
+    TraversalNode root;
+
+    TraversalNode getRoot() {
+        return root;
+    }
+
+    void setRoot(TraversalNode root) {
+        this.root = root;
+    }
+
+    /*노드를 만드는 메서드*/
+    void makeNode(int data, TraversalNode left, TraversalNode right){
+        TraversalNode node = new TraversalNode();
+        node.data = data;
+        node.leftNode  = left;
+        node.rightNode = right;
+    }
+
+    void inOrder(TraversalNode node){
+
+        if (node != null){
+            inOrder(node.leftNode);
+            System.out.println(node.data);
+            inOrder(node.rightNode);
+        }
+    }
+
+    void preOrder(TraversalNode node){
+        if(node != null){
+            System.out.println(node.data);
+            preOrder(node.leftNode);
+            preOrder(node.rightNode);
+        }
+    }
+
+    void postOrder(TraversalNode node){
+        if(node != null){
+            postOrder(node.leftNode);
+            postOrder(node.rightNode);
+            System.out.println(node.data);
+        }
+    }
 }
