dabe9d24059eb1d0aaae3ec59d6eccab1e3d5725
dhslrl321 <dhslrl321@gmail.com>
2020-07-10 17:49:28 +0900

    kakao

--- src/Test.java
+++ src/Test.java
@@ -1,63 +1,12 @@
 import java.util.*;
 
 public class Test {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        char[] s = sc.nextLine().toCharArray();
-        int len = s.length;
+        boolean result = a | b;
 
-        Stack<Character> stack = new Stack<Character>();
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < len; i++) {
-            int p = priority(s[i]);
-            char ch = s[i];
-
-            switch (ch) {
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                    while (!stack.isEmpty() && priority(stack.peek()) >= p) {
-                        sb.append(stack.pop());
-                    }
-                    stack.push(ch);
-                    break;
-                case '(':
-                    stack.push(ch);
-                    break;
-                case ')':
-                    while (!stack.isEmpty() && stack.peek() != '(') {
-                        sb.append(stack.pop());
-                    }
-                    stack.pop();
-                    break;
-                default:
-                    sb.append(ch);
-            }
-        }
-
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-
-        System.out.println(sb.toString());
+        System.out.println(Integer.parseInt(Boolean.toString(result)));
     }
-
-    public static int priority(char ch) {
-        switch (ch) {
-            case '*':
-            case '/':
-                return 2;
-            case '+':
-            case '-':
-                return 1;
-            case '(':
-            case ')':
-                return 0;
-        }
-        return -1;
-    }
-
 }
--- src/programmers/level1/Prob12_TreasureMap.java
+++ src/programmers/level1/Prob12_TreasureMap.java
@@ -4,33 +4,24 @@ import java.util.Arrays;
 
 public class Prob12_TreasureMap {
     public static void main(String[] args) {
-        int[] arr1 =
-        System.out.println(Arrays.toString(solution(5, )));
+        int[] arr1 = {46, 33, 33, 22, 31, 50};
+        int[] arr2 = {27, 56, 19, 14, 14, 10};
+        
     }
 
-    private static String[] solution(int n, int[] arr1, int[] arr2) {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
 
-        for(int i = 0; i < n; i++){
-            int bin1 = Integer.parseInt(Integer.toBinaryString(arr1[i])); // arr1 이진수 변환
-            int bin2 = Integer.parseInt(Integer.toBinaryString(arr2[i])); // arr2 이진수 변환
-
-            int oper = bin1 | bin2; // or 연산 결과 지도 생성
-
-            String[] map = Integer.toBinaryString(oper).split("");
-
-            StringBuilder sb = new StringBuilder();
-
-            for(int j = 0; j < map.length; j++){
-                if(map[j].equals("1")) sb.append("#");
-                else sb.append(" ");
-            }
-
-            answer[i] = sb.toString();
-
+        for (int i=0; i<n; i++) {
+            String temp = Integer.toBinaryString(arr1[i] | arr2[i]);
+            temp = String.format("%5s", temp);
+            answer[i] = temp.replaceAll("1", "#").replaceAll("0", " ");
         }
 
 
         return answer;
     }
 }
+/*
+* ######", "###  #", "##  ##", " #### ", " #####", "### # "
+* */
