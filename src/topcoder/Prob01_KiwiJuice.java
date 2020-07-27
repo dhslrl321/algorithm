52b52cd1ed682295e2a9e536398e0820620fadbb
dhslrl321 <dhslrl321@gmail.com>
2020-07-27 17:00:23 +0900

    kiwi

---
+++ src/topcoder/Prob01_KiwiJuice.java
@@ -0,0 +1,11 @@
+package topcoder;
+
+public class Prob01_KiwiJuice {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int[] capacities, int[] bottles, int[] fromId, int[] toId){
+        return null;
+    }
+}
