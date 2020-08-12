4ee81164e585964a2da0b505a7ee8be9ccc2e07a
dhslrl321 <dhslrl321@gmail.com>
2020-08-12 12:08:16 +0900

    dbf

--- src/Test.java
+++ src/Test.java
@@ -2,11 +2,27 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        boolean a = true;
-        boolean b = false;
 
-        boolean result = a | b;
+        List<String> nameList = new ArrayList<>();
+        List<String> nicknameList = new LinkedList<>();
 
-        System.out.println(Integer.parseInt(Boolean.toString(result)));
+        nameList.add("John");
+        nameList.add("Cameron");
+
+        nicknameList.add("Prim");
+        nicknameList.add("Stephan");
+
+        List<String> resultNameList = findUserInfo(nameList);
+        List<String> resultNicknameList = findUserInfo(nicknameList);
+
+        System.out.println(resultNameList);
+        System.out.println(resultNicknameList);
+
+    }
+
+    static List<String> findUserInfo(List<String> nameList) {
+        return new LinkedList<>();
     }
 }
+
+
