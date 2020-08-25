469b1d0749c840e883ba72a05098cb85e2d5c087
dhslrl321 <dhslrl321@gmail.com>
2020-08-26 03:12:49 +0900

    happy party

---
+++ src/topcoder/Prob02_HappyParty.java
@@ -0,0 +1,19 @@
+package topcoder;
+
+import java.util.Scanner;
+
+public class Prob02_HappyParty {
+    public static void main(String[] args) {
+        String[] arr1 = {"fishing", "fishing", "parking", "sounding"};
+        String[] arr2 = {"hunting", "hunting", "hunting", "singing"};
+
+        System.out.println("return: " + solution(arr1, arr2));
+    }
+
+    private static int solution(String[] arr1, String[] arr2){
+        int answer = 0;
+
+
+        return answer;
+    }
+}
