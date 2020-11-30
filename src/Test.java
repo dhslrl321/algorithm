ee6189ff6e3589702ac7fbfce0a1bac4e80a95f4
dhslrl321 <dhslrl321@gmail.com>
2020-11-30 10:04:26 +0900

    commit

--- src/Test.java
+++ src/Test.java
@@ -1,69 +1,12 @@
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
+        String a = "a0";
 
-        int input1 = 2;
-        int input2 = 2;
-        int input3 = 2;
-        int input4 = 2;
-
-        String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-!@#$%^&*()+=\\|/{}0123456789";
-
-        String flag = "where is medicine?";
-        String flag1 = "";
-        String flag2 = "";
-        String flag3 = "";
-        String flag4 = "";
-        String flag5 = "";
-
-        String cryptotext = str;
-
-        flag1 = str.substring(15, 16);
-        flag1 += cryptotext.substring(14, 15);
-        flag1 += cryptotext.substring(23, 24);
-        flag1 += cryptotext.substring(69, 70);
-
-
-        flag2 = cryptotext.substring(38, 39);
-        flag2 += cryptotext.substring(71, 72);
-        flag2 += cryptotext.substring(27, 28);
-        flag2 += cryptotext.substring(34, 35);
-        flag2 += cryptotext.substring(72, 73);
-        flag2 += cryptotext.substring(30, 31);
-        flag2 += cryptotext.substring(52, 53);
-
-
-
-
-        flag3 = cryptotext.substring(75, 76);
-        flag3 += cryptotext.substring(39, 40);
-        flag3 += cryptotext.substring(29, 30);
-        flag3 += cryptotext.substring(52, 53);
-
-
-
-        flag4 = cryptotext.substring(31, 32);
-        flag4 += cryptotext.substring(43, 44);
-        flag4 += cryptotext.substring(72, 73);
-        flag4 += cryptotext.substring(29, 30);
-        flag4 += cryptotext.substring(75, 76);
-        flag4 += cryptotext.substring(52, 53);
-
-        flag5 = cryptotext.substring(32, 33);
-        flag5 += cryptotext.substring(71, 72);
-        flag5 += cryptotext.substring(76, 77);
-        flag5 += cryptotext.substring(46, 47);
-        flag5 += cryptotext.substring(70, 71);
-
-        flag = flag1 + flag2 + flag3 + flag4 + flag5;
-
-        System.out.println();
-        System.out.println(flag);
-
-
-
+        System.out.println(a.charAt(0) - '0');
     }
 }
-
-
--- src/baekjoon/classification/class05_BruteForce/Prob12_NM1.java
+++ src/baekjoon/classification/class05_BruteForce/Prob12_NM1.java
@@ -1,29 +1,33 @@
 package baekjoon.classification.class05_BruteForce;
 
-import java.util.Arrays;
-import java.util.jar.JarOutputStream;
+import java.util.Scanner;
 
 public class Prob12_NM1 {
-    static boolean[] check = new boolean[10]; // index 가 10 까지니까
-    static int[] ans = new int[10]; // 몰라 아직
+    static boolean[] check = new boolean[10];
+    static int[] ans = new int[10];
     public static void main(String[] args) {
-        int n = 5;
-        int m = 3;
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int m = input.nextInt();
 
         go(0, n, m);
     }
 
     static void go(int index, int n, int m) {
-        if(index == m) { // index 가 반복하다가 조건에서 나온 m 까지 반복을 끝내면
-            System.out.println(Arrays.toString(ans));
-            return ;
+        if(index == m) {
+            for(int iter : ans) {
+                if(iter != 0) System.out.print(iter + " ");
+            }
+            System.out.println();
+            return;
         }
 
-        for (int i = 1; i <= n; i++) { // 숫자만큼의 배열 인덱스
+        for(int i = 1; i <= n; i ++) {
             if(check[i]) continue;
             check[i] = true; ans[index] = i;
             go(index + 1, n, m);
-            check[i] = false;
+            check[i] =false;
         }
     }
 }
