087b81c2d987660f7bfb3f57d5ce41d4054b6c40
dhslrl321 <dhslrl321@gmail.com>
2021-03-27 13:46:36 +0900

    commit-

---
+++ src/baekjoon/ps11simulation/P01_ColorPaper.java
@@ -0,0 +1,13 @@
+package baekjoon.ps11simulation;
+
+import java.io.*;
+
+public class P01_ColorPaper {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();
+        bw.close();
+    }
+}
--- src/baekjoon/ps11simulation/P01_King.java
+++
@@ -1,7 +0,0 @@
-package baekjoon.ps11simulation;
-
-public class P01_King {
-    public static void main(String[] args) {
-
-    }
-}
