0a21ad4e73fa6a640c90ecd851bea73b9fd10b64
dhslrl321 <dhslrl321@gmail.com>
2021-08-02 18:37:02 +0900

    commit

---
+++ src/solved/graph_search/B11725.java
@@ -0,0 +1,19 @@
+package solved.graph_search;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class B11725 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.flush();
+        bw.close();
+    }
+}
