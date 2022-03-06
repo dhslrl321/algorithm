bbb24813313280e7543accc539463a1cbee0a407
dhslrl321 <dhslrl321@gmail.com>
2022-03-06 16:59:09 +0900

    commit

--- src/solved/impl/B2504.java
+++ src/solved/impl/B2504.java
@@ -1,11 +1,15 @@
+package solved.impl;
+
 import java.io.*;
 import java.util.*;
 
 
-public class Main {
+public class B2504 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWrtier(System.out));
+
+
 
         bw.flush();
         bw.close();
