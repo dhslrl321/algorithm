ee08d4bd1e0d043bd5373d5849d435e9359e4c3b
dhslrl321 <dhslrl321@gmail.com>
2022-03-10 14:18:08 +0900

    commit

--- src/solved/impl/B1748.java
+++ src/solved/impl/B1748.java
@@ -8,8 +8,17 @@ public class B1748 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String nString = br.readLine();
         int n = Integer.parseInt(nString);
+
+        int answer = solutionB(n);
+
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+    }
+
+    private static int solutionA(int n) {
         int digit = (int) Math.log10(n) + 1; // 자리수
-        int count = 0;
+        int acc = 0;
         int step = 1;
 
         for (int i = 1; i < n; i *= 10) {
@@ -20,14 +29,26 @@ public class B1748 {
             int end = (i * 10) - 1;
             int temp = (end - start) + 1;
 
-            count += temp * step;
+            acc += temp * step;
             step++;
         }
 
-        count += (n - (Math.pow(10, digit - 1)) + 1) * digit;
+        acc += (n - (Math.pow(10, digit - 1)) + 1) * digit;
+        return acc;
+    }
 
-        bw.write(String.valueOf(count));
-        bw.flush();
-        bw.close();
+    private static int solutionB(int n) {
+        int step = 1;
+        int target = 10;
+        int acc = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % target == 0) {
+                step++;
+                target *= 10;
+            }
+            acc += step;
+        }
+
+        return acc;
     }
 }
